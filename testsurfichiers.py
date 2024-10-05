055b63ac821d4e4950d45bc8266d57e2022e6515
fknmgnrtr <leonhart.sebastien@gmail.com>
2024-10-05 12:27:00 -0400

    Jai encvore oublie

--- testsurfichiers.py
+++ testsurfichiers.py
@@ -1,7 +1,10 @@
-with  open("../ESP32/corbeau") as f:
+with  open("corbeau") as f:
     i=0
     for line in f:
         i+=1
         print(i,".",line)
     f.seek(0)
     print(f.readline(5))
+with  open("corbeau", "w") as f:
+    write("On commence a beaucoup changer le fichier")
+    
