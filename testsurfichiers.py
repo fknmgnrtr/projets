fdc0e1ec4470da5d98df54dcbd75c67c8e4bcac6
fknmgnrtr <leonhart.sebastien@gmail.com>
2024-10-05 12:37:26 -0400

    Revert "Jai encvore oublie"
    This reverts commit 055b63ac821d4e4950d45bc8266d57e2022e6515.

--- testsurfichiers.py
+++ testsurfichiers.py
@@ -1,10 +1,7 @@
-with  open("corbeau") as f:
+with  open("../ESP32/corbeau") as f:
     i=0
     for line in f:
         i+=1
         print(i,".",line)
     f.seek(0)
     print(f.readline(5))
-with  open("corbeau", "w") as f:
-    write("On commence a beaucoup changer le fichier")
-    
